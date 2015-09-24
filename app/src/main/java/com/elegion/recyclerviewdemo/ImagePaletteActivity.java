c45341af6a7535a1db7bfaf84261e09152637243
stepango <stepan.goncharov@e-legion.com>
2015-09-24 21:45:28 +0800

    Updated to 23.0.1 Support Lib

--- app/src/main/java/com/elegion/recyclerviewdemo/ImagePaletteActivity.java
+++ app/src/main/java/com/elegion/recyclerviewdemo/ImagePaletteActivity.java
@@ -18,9 +18,15 @@ public class ImagePaletteActivity extends Activity {
         setContentView(R.layout.ac_palette);
         BitmapDrawable bitmapDrawable = (BitmapDrawable) getWallpaper();
         ((ImageView) findViewById(R.id.image)).setImageDrawable(bitmapDrawable);
-        Palette palette = Palette.generate(bitmapDrawable.getBitmap());
+        Palette palette = Palette.from(bitmapDrawable.getBitmap()).generate();
         TextView text = (TextView) findViewById(R.id.text);
-        text.setTextColor(palette.getLightVibrantColor().getRgb());
-        text.setBackgroundColor(palette.getMutedColor().getRgb());
+        Palette.Swatch lightVibrantSwatch = palette.getLightVibrantSwatch();
+        if (lightVibrantSwatch != null) {
+            text.setTextColor(lightVibrantSwatch.getRgb());
+        }
+        Palette.Swatch mutedSwatch = palette.getMutedSwatch();
+        if (mutedSwatch != null) {
+            text.setBackgroundColor(mutedSwatch.getRgb());
+        }
     }
 }
